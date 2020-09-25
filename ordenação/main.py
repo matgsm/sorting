89d56faa1039bc58d83594a39874e1967dd3d838
matgsm <matheus.gsm.rb@gmail.com>
2020-09-25 00:52:50 -0300

    otimização:No de comparações e No de movimentações

--- ordenação/main.py
+++ ordenação/main.py
@@ -1,38 +1,44 @@
 from sorting import selection_sort, insertion_sort, mergesort, quicksort
-from Selects import ReadList, SelectMethod
+from Selects import ReadList, Print_ListName, SelectMethod, Print_Method
 import timeit
-
 import sys
+
 sys.setrecursionlimit(10**4)
-#quantidade de recursão padrao do python estava sendo excedida, portanto foi aumentada
+#quantidade de recursão padrao do python estava sendo excedida, portanto foi aumentada de 10^3 para 10^4
 
 if __name__ == "__main__":
 
-    lista = ReadList()  
-    choice = SelectMethod() 
+    lista = ReadList()          #retorna a lista escolhida entre as presentes em ./logs 
+    choice = SelectMethod()     #recebe o indice do metodo de ordenação desejado
     time=0.0
 
+    print ("\n\n\tOriginal:\n\n")
     print("\n*********************************************************************************************")
     for dados in lista:
-        print(dados)
+        print(dados)        #imprime a lista original
     print("*********************************************************************************************")
     
     #ordenando dados:
     
     time1 = timeit.default_timer()
     if ( (choice) == 0):
-      insertion_sort(lista)
+        CompMov = insertion_sort(lista)
     elif( (choice) == 1):
-        selection_sort(lista)
+        CompMov = selection_sort(lista)
     elif( (choice) == 2):
-        mergesort (lista)
+        CompMov = mergesort (lista)
     else:
-        quicksort(lista)
-    time2 = timeit.default_timer()
+        CompMov = quicksort(lista)
+    time2 = timeit.default_timer()      #(time2)-(time1) determina o tempo de ordenação independente do metodo
+
     print("\n\n\tOrdenado:\n\n")
     print("*********************************************************************************************")
     for dados in lista:
-        print(dados)
+        print(dados)        #imprime a lista ordenada
     print("*********************************************************************************************")
-    print("\ntempo para ordenar: ",(time2 - time1))
-
+    
+    print("\ntempo para ordenar: ",(time2 - time1))     #imprime o tempo para ordenação
+    Print_ListName()                                    #imprime o nome da lista
+    Print_Method(choice)                                #imprime o metodo de ordenação
+    print("\nComparações: ",CompMov[0])                 #imprime qtd de comparações
+    print("Movimentações: ",CompMov[1])                 #imprime qtd de movimentações
--- ordenação/sorting.py
+++ ordenação/sorting.py
@@ -1,82 +1,136 @@
-def quicksort(lista, inicio=0, fim=None):
-    if fim is None:
-        fim = len(lista)
-    if inicio < fim:
-        p = partition(lista, inicio, fim)
-        # recursivamente na sublista à esquerda (menores)
-        quicksort(lista, inicio, p)
-        # recursivamente na sublista à direita (maiores)
-        quicksort(lista, p+1, fim)
-
-def partition(lista, inicio, fim):
-    pivot = lista[inicio]
-    i = inicio + 1
-    for j in range( (inicio+1), fim ):
-        # j sempre avança, pois representa o elementa em análise
-        # e delimita os elementos maiores que o pivô
-        if lista[j] <= pivot:
-            lista[j], lista[i] = lista[i], lista[j]
-            # incrementa-se o limite dos elementos menores que o pivô
-            i = i + 1
-    lista[i-1], lista[inicio] = lista[inicio], lista[i-1]
-    return (i-1)
-
-#****************************************#
-
-def mergesort(lista, inicio=0, fim=None):
-    if fim is None:
-        fim = len(lista)
-    if(fim - inicio > 1):
-        meio = (fim + inicio)//2
-        mergesort(lista, inicio, meio)
-        mergesort(lista, meio, fim)
-        merge(lista, inicio, meio, fim)
-
-def merge(lista, inicio, meio, fim):
-    left = lista[inicio:meio]
-    right = lista[meio:fim]
-    top_left, top_right = 0, 0
-    for k in range(inicio, fim):
-        if top_left >= len(left):
-            lista[k] = right[top_right]
-            top_right = top_right + 1
-        elif top_right >= len(right):
-            lista[k] = left[top_left]
-            top_left = top_left + 1
-        elif left[top_left] < right[top_right]:
-            lista[k] = left[top_left]
-            top_left = top_left + 1
-        else:
-            lista[k] = right[top_right]
-            top_right = top_right + 1
+comp = 0    #quantidade de comparações
+movim = 0   #quantidade de movimentações entre registros
 
 #****************************************#
 
 def insertion_sort(lista):
+    global comp, movim
     n = len(lista)
-    for i in range(1, n):
+    for i in range(1, n):       #for
+        comp = comp + 1         #comp do laço for
         chave = lista[i]
         j = i - 1
-        while j >= 0 and lista[j] > chave:
+        while j >= 0 and lista[j] > chave:      #while
+            comp = comp + 2                     # 2 comparações no laço while
             lista[j+1] = lista[j]
+            movim = movim +1                    #movimentação
             j = j - 1
         lista[j+1] = chave
+        movim = movim + 1                       #movimentação
+    
+    CompMov = [ [comp],[movim] ]
+    return (CompMov)
+
 # Complexidade de tempo O(nˆ2)
 # Complexidade de espaço O(n)
 
 #****************************************#
 
 def selection_sort(lista):
+    global comp, movim
     n = len(lista)
-    for j in range(n-1):
+    for j in range(n-1):        #for-1
+        comp = comp + 1         #comp do laço for-1
         min_index = j
-        for i in range(j, n):
-            if lista[i] < lista[min_index]:
+        for i in range(j, n):   #for-2
+            comp = comp + 1     #comp do laço for-2
+            if lista[i] < lista[min_index]:     #if-1
+                comp = comp + 1                 #comp da condição if-1
                 min_index = i
-        if lista[j] > lista[min_index]:
-            aux = lista[j]
-            lista[j] = lista[min_index]
-            lista[min_index] = aux
+        if lista[j] > lista[min_index]:         #if-2
+            comp = comp + 1                     #comp da condição if-2
+            lista[j], lista[min_index] = lista[min_index], lista[j]
+            movim = movim +1                    #movimentação
+
+    CompMov = [ [comp],[movim] ]
+    return (CompMov)
+
 # 1 + (n-1)*[5 + X] = 1 + 5*(n-1) + X*(n-1)
 # Complexidade de tempo O(nˆ2)
 # Complexidade de espaço O(n)
+
+#****************************************#
+
+def mergesort(lista, inicio=0, fim=None):
+    global comp, movim
+    if fim is None:         #if-1
+        fim = len(lista)
+    comp = comp + 1         #comp condição if-1
+    if(fim - inicio > 1):   #if-2
+        meio = (fim + inicio) // 2    # '//' retorna a parte inteira da divisao
+        mergesort(lista, inicio, meio)
+        mergesort(lista, meio, fim)
+        merge(lista, inicio, meio, fim)
+    comp = comp + 1         #comp condição if-2
+    
+    CompMov = [ [comp],[movim] ]
+    return (CompMov)
+
+def merge(lista, inicio, meio, fim):
+    global comp, movim
+    left = lista[inicio:meio]
+    right = lista[meio:fim]
+    top_left, top_right = 0, 0
+    for k in range(inicio, fim):                    #for
+        comp = comp + 1                             #comp laço for
+        if top_left >= len(left):                   #if-3
+            lista[k] = right[top_right]
+            movim = movim + 1                       #movim
+            top_right = top_right + 1
+            comp = comp + 1                         #comp condição if-3
+        elif top_right >= len(right):               #elif-4
+            lista[k] = left[top_left]
+            movim = movim + 1                       #movim
+            top_left = top_left + 1
+            comp = comp + 2                         #(if-3) + (elif-4) = 2 comparações
+        elif left[top_left] < right[top_right]:     #(elif-5)
+            lista[k] = left[top_left]
+            movim = movim + 1                       #movim
+            top_left = top_left + 1
+            comp = comp + 3                         #(if-3) + (elif-4) + (elif-5) = 3 comparações
+        else:                                       #else-6
+            lista[k] = right[top_right]
+            movim = movim + 1                       #movim
+            top_right = top_right + 1
+            comp = comp + 3                         #(if-3) + (elif-4) + (elif-5) = 3 comparações
+                                                    #obs: else nao conta como comparação. Se comparações sao falsas, executa else
+
+
+#****************************************#
+
+def quicksort(lista, inicio=0, fim=None):
+    global comp, movim
+    if fim is None:     #if-1
+        fim = len(lista)
+    comp = comp+1       #comp condição if-1
+    if inicio < fim:    #if-2
+        p = partition(lista, inicio, fim)
+        # recursivamente na sublista à esquerda (menores)
+        quicksort(lista, inicio, p)
+        # recursivamente na sublista à direita (maiores)
+        quicksort(lista, p+1, fim)
+    comp = comp+1       #comp condição if-2
+
+    CompMov = [ [comp],[movim] ]
+    return (CompMov)
+    
+def partition(lista, inicio, fim):
+    global comp,movim
+    pivot = lista[inicio]
+    i = inicio + 1      #i:na esquerda tem menores que pivo; j:entre i e j tem  maiores que o pivo(direita do pivo)
+    for j in range( (inicio+1), fim ):  #for-1
+        comp = comp+1                   #comp laço for-1
+        # j sempre avança, pois representa o elementa em análise
+        # e delimita os elementos maiores que o pivô
+        if lista[j] <= pivot:           #if-3
+            lista[j], lista[i] = lista[i], lista[j]
+            movim = movim + 1           #movimentação de registros
+            # incrementa-se o limite dos elementos menores que o pivô
+            i = i + 1
+        comp = comp+1   #comp condição if-3
+    lista[i-1], lista[inicio] = lista[inicio], lista[i-1]   
+    movim = movim + 1                   #movimentação do pivô
+
+    return (i-1)
+
+#****************************************#
