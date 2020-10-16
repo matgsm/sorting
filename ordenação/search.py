bc2cebec2b84b98e237b7b575f42cc695db8475f
matgsm <matheus.gsm.rb@gmail.com>
2020-10-16 04:46:24 -0300

    update: comparações e tempo para Search

--- ordenação/search.py
+++ ordenação/search.py
@@ -1,22 +1,37 @@
+from Monitor import Monitor
 # Busca binária
-def binary_search (list, item, begin=0, end=None):
+def binary_search (list, item, begin=0, end=None, relatorio=None):
     if (end is None):
         end = len(list)-1
+        relatorio = Monitor()
+        relatorio.ind = None
+
     if (begin <= end):
         m = (begin + end)//2
+
+        relatorio.comp +=1
         if ( list[m][0].lower() == item.lower() ):
-            return (m)
+            relatorio.ind = m
+            return (relatorio)
+
+        relatorio.comp +=1
         if (item.lower() < list[m][0].lower() ):
-            return (binary_search(list, item, begin, m-1) )
+            return (binary_search(list, item, begin, m-1, relatorio) )
         else:
-            return (binary_search(list, item, m+1, end) )
-    return (None)
+            return (binary_search(list, item, m+1, end, relatorio) )
+    return (relatorio)
 
 
 def linear_search (list, item):
     end = len(list)
+    comp = 0
+    relatorio = Monitor()
     for i in range (0 , end ):
+        comp +=1
         if (list[i][0].lower() == item.lower() ):
-            return (i)
+            relatorio.comp = comp
+            relatorio.ind = i
+            return (relatorio)
 
-    return (None)   #Não esta na lista
+    relatorio.comp = comp
+    return (relatorio)   #Não esta na lista
